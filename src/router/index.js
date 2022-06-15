51bdb1ee5ee85144da53c46d194bef69b02dda84
daichunyu <daichunyu@corp.netease.com>
2022-06-15 09:49:54 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,5 @@
+import NProgress from 'nprogress' // progress bar
+
 import { createRouter, createWebHashHistory } from "vue-router";
 // import { createRouter, createWebHashHistory } from "./grouter/index"
 import Home from "../pages/home.vue";
@@ -8,5 +10,17 @@ const routes = [
   { path: "/about", name: "About", component: About },
 ];
 const router = createRouter({ history: createWebHashHistory(), routes });
+
+
+router.beforeEach(async (to, from, next) => {
+  // start progress bar
+  NProgress.start()
+  next();
+})
+
+router.afterEach(() => {
+  // finish progress bar
+  NProgress.done()
+})
 
 export default router;
